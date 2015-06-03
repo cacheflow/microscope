d617d9a6c7adf6b4500cbe2c06a5a10670b50e03
lexalexander10 <l.alexander10@gmail.com>
2015-06-02 17:48:34 -0700

    working on adding notifications.

--- server/publications.js
+++ server/publications.js
@@ -2,12 +2,13 @@ Meteor.publish('posts', function(){
   return Posts.find();
 });
 
-Meteor.publish('comments', function()
-    {
-        return Comments.find();
-    });
+Meteor.publish('comments', function(){
+  return Comments.find();
+});
 
-
+Meteor.publish("notifications", function(){
+  return Notifications.find();
+});
 
 
 
